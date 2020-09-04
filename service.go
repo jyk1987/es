93367030719782f030f3e7ad88b265724fff1265
甲鲲 <jykaxy@gmail.com>
2020-09-04 16:23:35 +0800

    完善服务调用，在服务方法报错后进行运行状态恢复.增加一个返回参数，代表服务方法是否执行成功

--- service.go
+++ service.go
@@ -88,7 +88,7 @@ func RegService(serviceInstance interface{}) {
 	eslog.Info("注册完毕:", service.Path)
 }
 
-func CallService(path, methodName string, params ...interface{}) ([]reflect.Value, error) {
+func CallService(path, methodName string, params ...interface{}) ([]reflect.Value, error, bool) {
 	// 获取服务
 	_ServicesLock.RLock()
 	s, ok := _Services[path]
@@ -97,34 +97,41 @@ func CallService(path, methodName string, params ...interface{}) ([]reflect.Valu
 	if !ok {
 		msg := "没有找到服务:" + path
 		eslog.Error(msg)
-		return nil, errors.New(msg)
+		return nil, errors.New(msg), false
 	}
 	//服务不包含方法
 	if s.Methods == nil {
 		msg := "服务" + path + "的方法为初始化"
 		eslog.Error(msg)
-		return nil, errors.New(msg)
+		return nil, errors.New(msg), false
 	}
 	//获取需要调用的方法
 	m, ok := s.Methods[methodName]
 	if !ok {
 		msg := "服务" + path + "中没有找到" + methodName + "方法"
 		eslog.Error(msg)
-		return nil, errors.New(msg)
+		return nil, errors.New(msg), false
 	}
 	if m.ParamCount != len(params) {
 		msg := "参数不匹配:" + path + "." + methodName + "参数为" + strconv.Itoa(m.ParamCount) +
 			"个,传入参数为" + strconv.Itoa(len(params)) + "个"
 		eslog.Error(msg)
-		return nil, errors.New(msg)
+		return nil, errors.New(msg), false
 	}
 	in := make([]reflect.Value, m.ParamCount) //创建参数集
 	for i := 0; i < m.ParamCount; i++ {
 		paramType := m.ParamsType[i]        //获取单个参数的类型
 		param := reflect.ValueOf(params[i]) //实例化传入的参数位reflect.Value类型
-		in[i] = param.Convert(paramType)    //转换参数为目标类型,增加兼容性
+		// TODO:实现不同类型的转换，防止报错
+		in[i] = param.Convert(paramType) //转换参数为目标类型,增加兼容性
 	}
+	defer func() {
+		err := recover()
+		if err != nil {
+			eslog.Error(path, ",", methodName, "调用失败")
+			eslog.Error(err)
+		}
+	}()
 	result := m.MethodInstance.Call(in)
-
-	return result, nil
+	return result, nil, true
 }
